d68db7134bd1899a3aa18c5ac20628963b48db52
Eric Fahendrena <ericfahendrena29@gmail.com>
2024-09-07 18:51:34 +0300

    added some details

--- README.md
+++ README.md
@@ -4,13 +4,15 @@ This API allows to get some important details of a specified token.
 
 ## Routes
 
-- `GET` : `https://solana-api-test.vercel.app/api/token/:token-addr/q-holders-info`
+- `GET` : `https://solana-api-test.vercel.app/api/token/:token-addr/market`
   
   ```json
   {
-    "Name":"[token name]",
-    "Number": "[number of address holding >= 0.10%]",
-    "Percentage": "[percentage]",
-    "Top 100 Percentage":"[top 100 percentage]"
+    "Name":"dogwifhat",
+    "Price": 1.532,
+    "Circulating Supply": 998845524.412126,
+    "Market Cap": 1530231343.3993769,
+    "Number of Address > 0.1%": 110,
+    "Top 100 Percentage": "63.27%"
   }
   ```
--- index.js
+++ index.js
@@ -7,17 +7,29 @@ const app = express();
 app.use(cors());
 
 const apiBaseURL = 'https://api.solana.fm';
-const holdersPageSize = 500;
+const holdersPageSize = 4000;
 
-var tokenAddr = '';
+var tokenAddr = 'EKpQGSJtjMFqKZ9KQanSqYXRcF8fBopzLHYxdM65zcjm';
+
+(async () => {
+  try {
+    const response = await axios.get(`https://api.solscan.io/token/${tokenAddr}`);
+    console.log(response.data);
+    // const data = {
+    //   tokenName: response.data.tokenList.name,
+    //   symbol: response.data.tokenList.symbol,
+    //   image: response.data.tokenList.image
+    // }
+
+    // return data;
+  } catch (err) {
+    console.log('Error:', err);
+  }
+})();
 
 const getTokenInfo = async () => {
   try {
-    const response = await axios.get(`${apiBaseURL}/v1/tokens/${tokenAddr}`, {
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
+    const response = await axios.get(`${apiBaseURL}/v1/tokens/${tokenAddr}`);
     const data = {
       tokenName: response.data.tokenList.name,
       symbol: response.data.tokenList.symbol,
@@ -37,7 +49,7 @@ const getCurrentSupply = async () => {
         'Content-Type': 'application/json'
       }
     });
-    return response.data.circulatingSupply;
+    return response.data;
   } catch (err) {
     console.log('Error:', err);
   }
@@ -67,7 +79,7 @@ const getQualHolders = async () => {
   let shouldStop = false;
   for (let i = 0; i < tokenAccounts.length; i++) {
     const tokenAmount = tokenAccounts[i].info.tokenAmount.uiAmount;
-    const perc = (tokenAmount * 100) / currSupply;
+    const perc = (tokenAmount * 100) / currSupply.circulatingSupply;
 
     if (perc < qualPerc) {
       shouldStop = true;
@@ -83,15 +95,50 @@ const getQualHolders = async () => {
   return qualHolders;
 }
 
-(() => {
+const getDailyPrice = async () => {
+  try {
+    const tokenInfo = await getTokenInfo();
+    let symbol = tokenInfo;
+    symbol = symbol.symbol.replace('$', '');
+    
+    const url = `https://min-api.cryptocompare.com/data/price?fsym=${symbol}&tsyms=usd`;
+    const response = await axios.get(url);
+    
+    const dtFeeds = await response.data;
 
-  app.get('/api/token/:tokenAddr/q-holders-info', async (req, res) => {
+    return dtFeeds;
+  } catch (err) {
+    console.log('Error:', err);
+  }
+}
+
+(async () => {
+  app.get('/api/token/:tokenAddr/market', async (req, res) => {
     tokenAddr = req.params.tokenAddr;
   
     const qualHolders = await getQualHolders();
     const tokenInfo = await getTokenInfo();
+    const price = await getDailyPrice();
+    const currSupply = await getCurrentSupply();
+    const mktCap = await (parseFloat(currSupply.circulatingSupply) * parseFloat(price.USD));
     const qhLength = qualHolders.length;
-    res.json({ 'Name': tokenInfo.tokenName, 'Number': qhLength, 'Percentage': qualHolders[qhLength-1][1], 'Top 100 Percentage': qualHolders[qhLength-1][2] });
+
+    res.json({ 
+      'Name': tokenInfo.tokenName,
+      'Price': price ? price.USD : null,
+      'Circulating Supply': currSupply.circulatingSupply,
+      'Market Cap': mktCap,
+      'Number of Address > 0.1%': qhLength,
+      'Top 100 Percentage': qualHolders[qhLength-1][2]
+    });
+  });
+
+  app.get('/api/token/:tokenAddr/market-overview', async (req, res) => {
+    tokenAddr = req.params.tokenAddr;
+    
+    const mktOverview = await getMarketOverview();
+
+    res.json(mktOverview);
   });
 })();
 
