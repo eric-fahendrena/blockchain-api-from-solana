f015702c316daf6b65f8897ec8564ad6c98cfd35
Eric Fahendrena <ericfahendrena29@gmail.com>
2024-09-07 09:29:55 +0300

    deploying on vercel

--- index.js
+++ index.js
@@ -95,7 +95,7 @@ const getQualHolders = async () => {
   });
 })();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log('Server running on port', port);
 });
